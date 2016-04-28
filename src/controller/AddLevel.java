068c4ea00ed795c9b9d03fcb65ef6ca2f967d0a8
Michael Biwer <mbiwer@madisoncollege.edu>
2016-04-28 14:37:53 -0500

    new bootstrap model, started getting CRUD hooked up

---
+++ src/controller/AddLevel.java
@@ -0,0 +1,41 @@
+package controller;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by Michael on 4/28/2016.
+ */
+@WebServlet(name = "AddLevel", urlPatterns = { "/html/AddLevel" } )
+
+public class AddLevel  extends HttpServlet {
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String url = "/html/AddLevel.html";
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+
+    }
+}
