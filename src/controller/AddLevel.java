b346b73f9d4efc285ee6b29e2b4eb8fe86f4e71b
Michael Biwer <mbiwer@madisoncollege.edu>
2016-04-29 15:09:54 -0500

    can now receive a full JSON object from servlet and display it correctly

--- src/controller/AddLevel.java
+++ src/controller/AddLevel.java
@@ -1,39 +1,62 @@
 package controller;
-        import javax.servlet.RequestDispatcher;
-        import javax.servlet.ServletException;
-        import javax.servlet.annotation.WebServlet;
-        import javax.servlet.http.HttpServlet;
-        import javax.servlet.http.HttpServletRequest;
-        import javax.servlet.http.HttpServletResponse;
-        import javax.servlet.http.HttpSession;
-        import java.io.IOException;
-        import java.text.ParseException;
-        import java.text.SimpleDateFormat;
-        import java.util.ArrayList;
-        import java.util.Date;
-        import java.util.List;
 
-        import org.apache.log4j.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.google.gson.Gson;
+import entity.Level1;
+import org.apache.log4j.Logger;
 
 /**
  * Created by Michael on 4/28/2016.
  */
-@WebServlet(name = "AddLevel", urlPatterns = { "/html/AddLevel" } )
-public class AddLevel  extends HttpServlet {
+@WebServlet(name = "AddLevel", urlPatterns = {"/html/AddLevel"})
+public class AddLevel extends HttpServlet {
+    private static final long serialVersionUID = 1L;
     private final Logger log = Logger.getLogger(this.getClass());
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String url = "/html/AddLevel.html";
 
         log.info("1.0 got into servlet somehow ?");
 
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
-        dispatcher.forward(req, resp);
+        dispatcher.forward(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        log.info("2.0 got into servlet somehow ?");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Level1 level1 = new Level1();
+        level1.setLevelOneID(1);
+        level1.setListingName("Level 1 Listing Name");
+        level1.setListingDescription("Level 1 Listing Description");
+        level1.setBookName("Level 1 Book Name");
+        level1.setPageNumber(1);
+        level1.setVersionID(1);
+
+//        response.setContentType("application/json");
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+
+        String json = new Gson().toJson(level1);
+        out.println(json);
+        out.close();
+
+        log.info("{success : 'Yes'}");
+        log.info(request.getReader());
+        log.info(json.toString());
     }
 }
--- web/html/myApp.js
+++ web/html/myApp.js
@@ -1,11 +1,45 @@
 var app = angular.module('myApp', []);
 
-app.controller('myCtrl', function ($scope, $http) {
+app.controller('myCtrl', ['$scope', '$http', function ($scope, $http){
 
-    this.sendPost = function () {
+    this.sendGet = function () {
         $http.get("http://localhost:8080/html/AddLevel").then(function (response) {
-            alert("Controller Hit" + response);
+            alert("Controller Hit" + response.toString());
         });
     }
-    
-});
+
+    this.sendPost = function () {
+        $http.post("http://localhost:8080/html/AddLevel", "Stuff").success(function (response) {
+
+            alert("Controller Hit " + data + " " + status);
+        });
+    }
+
+    this.getDataFromServer = function() {
+
+        $http({
+            method : 'POST',
+            url : 'http://localhost:8080/html/AddLevel',
+        }).success(function(response) {
+            var data = angular.fromJson(response);
+            $scope.parsedData = data;
+
+            $scope.levelOneID = data.levelOneID;
+            $scope.listingName = data.listingName;
+            $scope.listingDescription = data.listingDescription;
+            $scope.versionID = data.versionID;
+            $scope.bookName = data.bookName;
+            $scope.pageNumber = data.pageNumber;
+
+            $scope.response = response;
+
+            alert(response);
+        }).error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+    };
+
+
+}]);
